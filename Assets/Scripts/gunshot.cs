12a02586b54b2015b10654f46d2069c86e4588a4
npietrafesa <npietrafesa110@gmail.com>
2020-05-01 23:51:37 -0400

    tweaks

--- Assets/Scripts/gunshot.cs
+++ Assets/Scripts/gunshot.cs
@@ -5,27 +5,29 @@ using UnityEngine;
 class gunshot : MonoBehaviour {
     public AudioSource gunfire;
     public Animation Gunshot;
-    public Animation ADSM9;
     int ammo = 15;
     void Start() {
         gunfire = GetComponent<AudioSource>();
         Gunshot = GetComponent<Animation>();
-       // ADSM9 = GetComponent<Animation>();
     }
 
     void Update() {
-        if (Input.GetKeyDown(KeyCode.Mouse0) && !Gunshot.isPlaying && ammo != 0) {
+        if (Input.GetKeyDown(KeyCode.Mouse0) && !Gunshot.IsPlaying("Gunshot") && !Gunshot.IsPlaying("ADSM9")&&  ammo != 0) {
             gunfire.Play();
             Gunshot.Play("Gunshot");
             ammo--;
         }
-        if (Input.GetKeyDown(KeyCode.Mouse1))
-        {
+
+        if (Input.GetMouseButton(1)) {
             Gunshot.Play("ADSM9");
+            if (!Input.GetMouseButton(1)) {
+                Gunshot["ADSM9"].speed = -1;
+                Gunshot.Play("ADSM9");
+                Gunshot["ADSM9"].speed = 1;
+            }
+ 
         }
-        else if (Input.GetKeyUp(KeyCode.Mouse1)) {
-            Gunshot.Rewind("ADSM9");
-        }
+
 
         if (Input.GetKeyDown(KeyCode.R) && ammo == 0) {
             ammo = 15;

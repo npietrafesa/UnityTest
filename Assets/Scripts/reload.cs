adafd43b88da9230d60e4a70c5d5aed2b5416432
npietrafesa <npietrafesa110@gmail.com>
2020-05-02 22:57:39 -0400

    reload tweaks

---
+++ Assets/Scripts/reload.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class reload : MonoBehaviour
+{
+   // public AudioSource ReloadSound;
+    public GameObject CrossObject;
+    public GameObject MechanicsObject;
+    public int ClipCount;
+    public int ReserveCount;
+    public int ReloadAvailable;
+    public gunshot GunComponent;
+
+    void Start()
+    {
+        GunComponent = GetComponent<gunshot>();
+    }
+
+    void Update()
+    {
+        ClipCount = GlobalAmmo.LoadedAmmo;
+        ReserveCount = GlobalAmmo.CurrentAmmo;
+
+        if (ReserveCount == 0)
+        {
+            ReloadAvailable = 0;
+        }
+        else
+        {
+            ReloadAvailable = 15 - ClipCount;
+        }
+
+        if (Input.GetButtonDown("Reload"))
+        {
+            if (ReloadAvailable >= 1)
+            {
+                if (ReserveCount <= ReloadAvailable)
+                {
+                    GlobalAmmo.LoadedAmmo += ReserveCount;
+                    GlobalAmmo.CurrentAmmo -= ReserveCount;
+                    ActionReload();
+                }
+                else
+                {
+                    GlobalAmmo.LoadedAmmo += ReloadAvailable;
+                    GlobalAmmo.CurrentAmmo -= ReloadAvailable;
+                    ActionReload();
+                }
+            }
+            StartCoroutine(EnableScripts());
+
+        }
+    }
+
+    IEnumerator EnableScripts()
+    {
+        yield return new WaitForSeconds(1.1f);
+        GunComponent.enabled = true;
+        CrossObject.SetActive(true);
+        MechanicsObject.SetActive(true);
+    }
+
+    void ActionReload()
+    {
+        GunComponent.enabled = false;
+        CrossObject.SetActive(false);
+        MechanicsObject.SetActive(false);
+        //ReloadSound.Play();
+        GetComponent<Animation>().Play("m9reload");
+    }
+}
+

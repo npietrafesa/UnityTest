2c0fcd2a4631982854cdfc78c74e13ca21963bc4
npietrafesa <npietrafesa110@gmail.com>
2020-05-03 21:08:37 -0400

    muzzle flash + pickup

--- Assets/Scripts/gunshot.cs
+++ Assets/Scripts/gunshot.cs
@@ -5,32 +5,30 @@ using UnityEngine;
 public class gunshot : MonoBehaviour {
     public AudioSource gunfire;
     public Animation Gunshot;
+    public GameObject Flash;
     //int ammo = 15;
     void Start() {
         gunfire = GetComponent<AudioSource>();
         Gunshot = GetComponent<Animation>();
-        
     }
 
     void Update() {
         if (Input.GetKeyDown(KeyCode.Mouse0) && !Input.GetMouseButton(1) && !Gunshot.IsPlaying("Gunshot") && GlobalAmmo.LoadedAmmo != 0) {
             gunfire.Play();
+            Flash.SetActive(true);
+            StartCoroutine(MuzzleOff());
             Gunshot.Play("Gunshot");
-            //ammo--;
-            //GlobalAmmo.CurrentAmmo = ammo;
             GlobalAmmo.LoadedAmmo -= 1;
         } else if (Input.GetKeyDown(KeyCode.Mouse0) && Input.GetMouseButton(1) && !Gunshot.IsPlaying("adsgunshot") && GlobalAmmo.LoadedAmmo != 0) {
             gunfire.Play();
+            Flash.SetActive(true);
+            StartCoroutine(MuzzleOff());
             Gunshot.Play("adsgunshot");
-            //ammo--;
-            //GlobalAmmo.CurrentAmmo = ammo;
             GlobalAmmo.LoadedAmmo -= 1;
         }
-      /*  if (Input.GetKeyDown(KeyCode.R) && !Gunshot.IsPlaying("Gunshot") && !Gunshot.IsPlaying("adsgunshot")  && GlobalAmmo.LoadedAmmo < 15) {
-            Gunshot.Play("m9reload");
-            //ammo = 15;
-            //GlobalAmmo.CurrentAmmo = ammo;
-            GlobalAmmo.LoadedAmmo = 15;
-        } */
+    }
+    IEnumerator MuzzleOff() {
+        yield return new WaitForSeconds(.15f);
+        Flash.SetActive(false);
     }
 }
